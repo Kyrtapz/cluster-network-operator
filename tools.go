7f04198f05b74ec0915f0c53bf287a9b7ab6ff68
Arnab Ghosh <arnabghosh89@gmail.com>
2024-04-05 16:16:12 +0530

    API bump in CNO
    Change log:
    
    - Bump API after API PR merged
    $ go get github.com/openshift/api
    $ go mod tidy
    
    - Created tools.go in parent directory to be able to vendor API changes
    into CNO repository.
    $ go mod vendor
    
    - Updated hack/update-codegen.sh to mention correct directory and
    filename
    $ hack/update-codegen.sh
    
    Signed-off-by: Arnab Ghosh <arnabghosh89@gmail.com>

---
+++ tools.go
@@ -0,0 +1,13 @@
+//go:build tools
+// +build tools
+
+package main
+
+// openshift/api changed where generated CRD manifests are tracked. This import
+// is now required to get the CRD manifests vendored
+import (
+	_ "github.com/openshift/api/cloudnetwork/v1/zz_generated.crd-manifests"
+	_ "github.com/openshift/api/networkoperator/v1/zz_generated.crd-manifests"
+	_ "github.com/openshift/api/operator/v1/zz_generated.crd-manifests"
+	_ "github.com/openshift/api/config/v1/zz_generated.crd-manifests"
+)
